cb9dfc31f6d91aa8c46d3e23f10e2d0904a399d6
RishiMandhani <147861098+RishiMandhani@users.noreply.github.com>
2024-09-04 19:39:59 +0000

    comparison

--- basic/comparison.js
+++ basic/comparison.js
@@ -7,5 +7,14 @@ console.log(null==0);
 //itchecks datatype also
 console.log("2"===2);
 //this give false as these both are not same data type
+//data types
+//primitive
+//Number,String,null,undefined,Bollean,Symbol,BigInt,7 types
+//non primitive(refference)
+//Array,Objects,Functions
+//Symbol
+const Id=Symbol('123')
+const another=Symbol('123')
+console.log(Id==another);//it give value as false
 
 
