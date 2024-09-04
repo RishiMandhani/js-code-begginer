5feb172fa4eac5e7f5ff92502dafdd1fb54b4424
RishiMandhani <147861098+RishiMandhani@users.noreply.github.com>
2024-09-04 18:58:02 +0000

    operations

---
+++ basic/operations.js
@@ -0,0 +1,19 @@
+let value=3
+let negvalue= -value
+console.log(negvalue);
+let str1="rishi"
+let str2=" hello"
+let str3=str1 +str2
+console.log(str3);
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log(+"");
+console.log(+true);
+let num1,num2,num3
+num1=num2=num3=2+2
+
+
+
+
+
+
