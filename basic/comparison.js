fe08635f92e5d5009eff1cf0c7c42b91496ab98e
RishiMandhani <147861098+RishiMandhani@users.noreply.github.com>
2024-09-04 19:09:04 +0000

    comparison

---
+++ basic/comparison.js
@@ -0,0 +1,11 @@
+console.log(2>=3);
+console.log("2">1);
+console.log(null>0);
+console.log(null>=0);
+console.log(null==0);
+//=== strict check
+//itchecks datatype also
+console.log("2"===2);
+//this give false as these both are not same data type
+
+
