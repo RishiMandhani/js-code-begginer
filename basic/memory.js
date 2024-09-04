eaf2b44f0ca6f84caeccdcebb4687aa1ed55e0a9
RishiMandhani <147861098+RishiMandhani@users.noreply.github.com>
2024-09-04 20:14:20 +0000

    important memory concept

---
+++ basic/memory.js
@@ -0,0 +1,17 @@
+//Heap(non primitive),Stack(primitive) memory
+let myname="rishi"
+let anothername=myname
+anothername="Chai or code"
+let userdata1={
+    email:"user1@gmail.com"
+}
+let userdata2=userdata1
+console.log(userdata1);
+console.log(userdata2);
+
+
+userdata2.email="rishi@gmial.com"
+console.log(userdata1);
+console.log(userdata2);
+//ek baar run karana
+
